c5c139781c694bb1295c2324d7feba7df7b5bee1
Lio <c29nathan.lio@afacademy.af.edu>
2025-09-18 10:57:35 -0600

    New questions

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ file_path = script_dir / "questions.txt"
 # For this example, I'll create a few and multiply them.
 # You should replace these with your actual questions.
 question_list = []
+num_qs = 0
 with open(file_path) as fil:
     for line in fil:
         question_list.append(str(line))
+        num_qs +=1
     fil.close()
 # Let's make the list 100 questions long for this demo
 questions = question_list
@@ -20,12 +22,12 @@ def home():
     result_text = ""
     if request.method == 'POST':
         # This is the new logic to tally the 'yes' answers
-        score = 100
+        score = 0
         for value in request.form.values():
             if value == 'yes':
-                score -= 1
+                score += 1
         
-        result_text = f"Your Score: {score}"
+        result_text = f"Your Score: {round(1-(score/num_qs),1)*100}"
 
     # We pass the list of questions to the HTML template
     return render_template('index.html', questions=questions, result=result_text)
